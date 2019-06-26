9c6556cb562c8aa3d6fe53054a88bc7a21d71dd5
Grzegorz Swiec <grzegorz@radixdlt.com>
2019-06-26 12:12:00 +0200

    test configuration: 1298 nodes / 1298 shards

--- tools/libcloud/config.py
+++ tools/libcloud/config.py
@@ -2,9 +2,8 @@ import os
 import logging
 import argparse
 
-
 # cloudinit
-BASE_CLOUD_INIT_PATH=os.path.join(
+BASE_CLOUD_INIT_PATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     "..",
     "cloudinit")
@@ -15,12 +14,12 @@ DEFAULT_CREDS = '~/.gcloud.json'
 PROJECT = os.getenv('RADIX_MTPS_CLOUD_PROJECT', 'fast-gateway-233909')
 
 # network config
-DEFAULT_NETWORK_UNIVERSE=""
-DEFAULT_NETWORK_PASSWORD="DEFAULT_PASSWORD"
-DEFAULT_NETWORK_ATOMS_FILE="/radix/atoms_v4_full.zst"
-DEFAULT_NETWORK_START_PUMPING=40 # X minutes
-DEFAULT_NETWORK_SHARD_COUNT="1181"
-DEFAULT_NETWORK_SHARD_OVERLAP="0.1"
+DEFAULT_NETWORK_UNIVERSE = ""
+DEFAULT_NETWORK_PASSWORD = "DEFAULT_PASSWORD"
+DEFAULT_NETWORK_ATOMS_FILE = "/radix/atoms_v4_full.zst"
+DEFAULT_NETWORK_START_PUMPING = 40  # X minutes
+DEFAULT_NETWORK_SHARD_COUNT = "1298"
+DEFAULT_NETWORK_SHARD_OVERLAP = "0.1"
 
 # cores
 CORE_CLOUD_INIT = os.path.join(BASE_CLOUD_INIT_PATH, "core")
@@ -30,13 +29,13 @@ CORE_MACHINE_BOOT_INSTANCE_TEMPLATE_NAME = "core-boot-template"
 CORE_MACHINE_INSTANCE_TEMPLATE_NAME = "core-template"
 CORE_MACHINE_SIZE = "n1-standard-8"
 CORE_MACHINE_IMAGE = "projects/fast-gateway-233909/global/images/ubuntu-1604-lts-radix-1mtps"
-CORE_MACHINE_STORAGE = "100" # GB
+CORE_MACHINE_STORAGE = "100"  # GB
 CORE_EXTRA_DISK_IMAGE_NAME = "projects/fast-gateway-233909/global/images/atoms-v4-full-standard-persistent-image3"
-CORE_EXTRA_DISK_SIZE = "200" # GB
+CORE_EXTRA_DISK_SIZE = "200"  # GB
 CORE_REGIONS = {
     # Europe
     # 130 IP, 1000 CPU, 25TB, 4GB HDD, MAX 125 nodes
-    "europe-north1": 155,
+    "europe-north1": 125,
     # 110 IP, 1000 CPU, 24TB
     "europe-west4": 110,
     # 110 IP, 1000 CPU, 24TB - 3 vm, bottleneck: SSD_TOTAL_GB,MAX 104 nodes
@@ -46,40 +45,40 @@ CORE_REGIONS = {
     # 8 IP, 24 CPU, 4TB, MAX 2 nodes
     "europe-west3": 2,
     # 8 IP, 24 CPU, 4TB, MAX 2 nodes
-    "europe-west6":2,
+    "europe-west6": 2,
 
-    #Americas
+    # Americas
     # 130 IP, 1000 CPU, 23TB, MAX 115 nodes
-    "us-central1":115,
+    "us-central1": 115,
     # 110 IP, 1000 CPU, 22TB
-    "us-west1":2,
+    "us-west1": 110,
     # 110 IP, 1000 CPU, 22TB
-    "us-east1":110,
+    "us-east1": 110,
     # 100 IP, 1000 CPU, 20.48TB
-    "us-east4":100,
+    "us-east4": 100,
     # 8 IP, 24 CPU, 4TB, MAX 2 nodes
-    "us-west2":2,
+    "us-west2": 2,
 
     # 100 IP, 1000 CPU, 25TB, DISK_TOTAL_GB MAX 100 nodes - 1 node
-    "northamerica-northeast1":99,
+    "northamerica-northeast1": 99,
     # 8 IP, 24 CPU, 4TB, MAX 2 nodes:
-    "southamerica-east1":2,
+    "southamerica-east1": 2,
 
-    #Asia
+    # Asia
     # 130 IP, 1000 CPU, 20.48TB, bottleneck: SSD's, MAX 102 nodes
-    "asia-northeast1":102,
+    "asia-northeast1": 102,
     # 100 IP, 1000 CPU, 20.48TB - bottleneck: IP's, MAX 100 nodes
-    "asia-southeast1":100,
+    "asia-southeast1": 100,
     # 100 IP, 1000 CPU, 20.48TB
-    "asia-east1":100,
+    "asia-east1": 100,
     # 8 IP, 24 CPU, 4TB, MAX 2 nodes
-    "asia-east2":2,
+    "asia-east2": 2,
     # 8 IP, 24 CPU, 4TB, MAX 2 nodes
-    "asia-south1":2,
+    "asia-south1": 2,
 
-    #Australia
+    # Australia
     # 8 IP, 400 CPU, MAX 2 nodes
-    #"australia-southeast1":2
+    "australia-southeast1": 2
 }
 
 # explorer
@@ -88,44 +87,45 @@ EXPLORER_MACHINE_INSTANCE_NAME = "explorer"
 EXPLORER_MACHINE_ZONE = "europe-west3-a"
 EXPLORER_MACHINE_SIZE = "n1-standard-8"
 EXPLORER_MACHINE_IMAGE = "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1604-xenial-v20190530c"
-EXPLORER_MACHINE_STORAGE = "10" # GB
+EXPLORER_MACHINE_STORAGE = "10"  # GB
 
 # test prepper
 TEST_PREPPER_MACHINE_INSTANCE_NAME = "dataset-preparator"
 TEST_PREPPER_MACHINE_IMAGE = "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1604-xenial-v20190530c"
-TEST_PREPPER_MACHINE_SIZE="n1-standard-8"
-TEST_PREPPER_MACHINE_ZONE="europe-west3-a"
-TEST_PREPPER_MACHINE_STORAGE="1536" # GB
+TEST_PREPPER_MACHINE_SIZE = "n1-standard-8"
+TEST_PREPPER_MACHINE_ZONE = "europe-west3-a"
+TEST_PREPPER_MACHINE_STORAGE = "1536"  # GB
 TEST_PREPPER_CLOUD_INIT = os.path.join(BASE_CLOUD_INIT_PATH, "testprepper")
 
 # logging
 logging.basicConfig(
-        format = "%(message)s",
-        level = os.environ.get('LOGLEVEL', 'INFO').upper())
+    format="%(message)s",
+    level=os.environ.get('LOGLEVEL', 'INFO').upper())
 logging.getLogger("paramiko").setLevel(logging.WARNING)
+
 
 # arguments
 def get_arguments(parser):
     parser.add_argument(
         "--destroy-explorer",
-        action = "store_true",
-        help = "Delete the explorer node of the network.")
+        action="store_true",
+        help="Delete the explorer node of the network.")
     parser.add_argument(
         "--destroy-cores",
-        action = "store_true",
-        help = "Delete the core nodes of the network.")
+        action="store_true",
+        help="Delete the core nodes of the network.")
     parser.add_argument(
         "--destroy-firewall-rules",
-        action = "store_true",
-        help = "Delete the firewall rules of the network.")
+        action="store_true",
+        help="Delete the firewall rules of the network.")
     parser.add_argument(
         "--create-test-prepper",
-        action = "store_true",
-        help = "Create a specific machine to generate the bitcoin dataset.")
+        action="store_true",
+        help="Create a specific machine to generate the bitcoin dataset.")
     parser.add_argument(
         "--destroy-test-prepper",
-        action = "store_true",
-        help = "Destroy the machine to generate the bitcoin dataset.")
+        action="store_true",
+        help="Destroy the machine to generate the bitcoin dataset.")
     return parser.parse_args()
 
 
